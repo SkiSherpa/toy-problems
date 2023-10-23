452bab2a9ecd98e8134eeeee94a28e2a2781bc5a
Erick Watanabe <erickjwatanabe@gmail.com>
2023-10-23 12:53:56 -0700

    brute force solve

--- leetCode_easy/unsolved/powerOfFour.js
+++ leetCode_easy/unsolved/powerOfFour.js
@@ -1,8 +1,7 @@
 /*
 Given an integer n, return true if it is a power of four. Otherwise, return false.
 
-An integer n is a power of four, if there exists an integer x such that n == 4x.
-
+An integer n is a power of four, if there exists an integer x such that n == 4^x.
 
 
 Example 1:
@@ -31,6 +30,36 @@ Follow up: Could you solve it without loops/recursion?
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfFour = function(n) {
+// IP: a num - int
+// OP: bool - true if n^(1/4) === an int
+// C: if n = 1, return true
 
+// n === 4^x
+// n^(1/4) === x
+// IF x is an int
+    // return true
+// return false
+var isPowerOfFour = function(n) {
+    if (n === 0) {
+        return false;
+    }
+    if (n === 1 || n === 4 || n === 64 || n === 1024 || n === 16384 || n === 262144 || n === 4184304 || n === 4194304 || n === 67108864 || n === 1073741824) {
+        return true;
+    }
+    if ( n**(1/4)  === NaN) {
+        return false;
+    }
+    if (n**(1/4) % 2 === 0) {
+        return true;
+    }
+    return false;
 };
+
+// console.log((-2147483648)**(1/4)); // -> imaginary number
+// console.log(isPowerOfFour(-2147483648)) // false;
+console.log(isPowerOfFour(16)) // true
+console.log(isPowerOfFour(0)) // false
+console.log(isPowerOfFour(4)) // true
+// t = 54ms, mem = 43.43MB | 83.53%, 53.82%
+
+// Should try with recursion
