0732b615c30e23f33299d80c62a61713196f9962
Erick Watanabe <erickjwatanabe@gmail.com>
2023-10-30 15:52:58 -0700

    written some test cases

--- leetCode_easy/singleNum.js
+++ leetCode_easy/singleNum.js
@@ -32,3 +32,7 @@ Each element in the array appears twice except for one element which appears onl
 var singleNumber = function(nums) {
 
 };
+
+console.log(singleNumber([2,2,1])) // 1
+console.log(singleNumber([4,1,2,1,2])) // 4
+console.log(singleNumber([1])) // 1
