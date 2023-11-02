be52994ea0c69b7c1d67b9a78e1c5d038b386e8a
Erick Watanabe <erickjwatanabe@gmail.com>
2023-11-02 16:36:21 -0700

    optimized for memory and time

--- leetCode_easy/majorityElement.js
+++ leetCode_easy/majorityElement.js
@@ -33,18 +33,20 @@ n == nums.length
     // add current numbers to counts and count occurances
 // IF the count of a number is >= majority
     // return that number
+// FOR BEST TIME:
 var majorityElement = function(nums) {
-    const maj = Math.ceil(nums.length/2);
+    const len = nums.length;
+    const maj = Math.ceil(len/2);
     let counts = {};
-    for (let i = 0; i < nums.length; i++) {
-
-        if (counts[nums[i]]) {
-            counts[nums[i]]++;
+    for (let i = 0; i < len; i++) {
+        let cur = nums[i];
+        if (counts[cur]) {
+            counts[cur]++;
         } else {
-            counts[nums[i]] = 1;
+            counts[cur] = 1;
         }
-        if (counts[nums[i]] >= maj) {
-            return nums[i];
+        if (counts[cur] >= maj) {
+            return cur;
         }
     }
 };
@@ -52,4 +54,26 @@ var majorityElement = function(nums) {
 console.log(majorityElement([3,2,3])); // 3
 console.log(majorityElement([2,2,1,1,1,2,2])); // 2
 
+// FOR BEST MEMORY:
+var _majorityElement = function(nums) {
+    const maj = Math.ceil(nums.length/2);
+    let counts = {};
+    for (let i = 0; i < nums.length; i++) {
+        let cur = nums[i];
+        if (counts[cur]) {
+            counts[cur]++;
+        } else {
+            counts[cur] = 1;
+        }
+        if (counts[cur] >= maj) {
+            return cur;
+        }
+    }
+};
 // t =68ms | mem = 44.18MB; 18.43% | 48.75%
+// added in cur in loop
+// t = 64ms | mem = 43.62MB; 31.9% | 65.58%
+// add len var instead of nums.length in for
+// t = 59ms | 43.96MB; 55.98% | 53.93%
+// added len to the "maj" calcuation
+// t = 48ms | mem = 44.29MB; 94.82% | 46.87%
