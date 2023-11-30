afd47634afaf4ded73767f7b4080b3a429d8600d
Erick Watanabe <erickjwatanabe@gmail.com>
2023-11-30 15:18:28 -0800

    added new problem to practice with the stack structure

---
+++ leetCode_easy/validPairs.js
@@ -0,0 +1,11 @@
+// Write a function that can tell if ()'s are properly closed. Only using ()
+// practice using the stack data structure
+// its valid if it works like math parenths
+let validPairs = function (s) {
+
+}
+
+console.log(validPairs('()')); // true
+console.log(validPairs('(())')); // true
+console.log(validPairs('(()')); // false
+console.log(validPairs('(()()()(()))')); // true
