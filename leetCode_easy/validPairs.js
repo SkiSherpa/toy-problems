43e5f56da643e5317cf22b0c38bed11b19e82c84
Erick Watanabe <erickjwatanabe@gmail.com>
2023-11-30 15:46:17 -0800

    added comments to understand further

--- leetCode_easy/validPairs.js
+++ leetCode_easy/validPairs.js
@@ -7,7 +7,7 @@
     // IF current is an (
         // push it onto the stack
     // otherwise, IF a clossing )
-        // IF the stack is empty OR ... I'm not sure whats happening
+        // IF the stack is empty AND ... I'm not sure whats happening
             // return false
         // if it makes it passed ^^, then it matches
         // want to remove the open ( from the stack
@@ -15,10 +15,30 @@
 
 let validPairs = function (s) {
     let stack = [];
-
+    for (let i = 0; i < s.length; i++) {
+        let c = s[i];
+        if (c === '(') {
+            stack.push('(');
+        } else {
+            console.log(stack[stack.length - 1], stack, "c: ", c);
+            // handling if the stack is empty and a ) comes up
+            // OR
+            // handling too many ), prompts early return
+            // getting the last item in the stack: stack[stack.length - 1]
+            if (stack.length === 0 || stack[stack.length - 1] === ')') {
+                console.log('early return of false')
+                return false;
+            }
+            stack.pop();
+        }
+    }
+    console.log('made it to the end, stack legnth: ', stack.length)
+    return !stack.length;
 }
 
-console.log(validPairs('()')); // true
-console.log(validPairs('(())')); // true
-console.log(validPairs('(()')); // false
-console.log(validPairs('(()()()(()))')); // true
+// console.log(validPairs('()')); // true
+// console.log(validPairs('(())')); // true
+// console.log(validPairs('(()')); // false
+// console.log(validPairs('(()()()(()))')); // true
+// console.log(validPairs('(()))()')); // false
+console.log(validPairs(')')); // false
