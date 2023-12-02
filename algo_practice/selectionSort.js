6814ae5aa6e49afe34c3ddbdb8e3dc3e933dfb2f
Erick Watanabe <erickjwatanabe@gmail.com>
2023-12-01 16:03:12 -0800

    selection sort

---
+++ algo_practice/selectionSort.js
@@ -0,0 +1,29 @@
+/*
+Have 2 pointers: lowest & the val at arr[j]
+lowest - current lowest val index
+arr[j] - inner loop index , looking for the smallest value
+
+[3, 5, 1, 2]
+
+*/
+
+
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      let lowest = i
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[lowest]) {
+            console.log("IF trigger lowest ", lowest);
+          lowest = j
+          console.log("new lowest: ", lowest);
+        }
+      }
+      if (lowest !== i) {
+        // Swap
+        [arr[i], arr[lowest]] = [arr[lowest], arr[i]];
+      }
+    }
+    return arr
+  }
+  console.log(selectionSort([3, 5, 1, 2])) // [1, 2, 3, 5]
