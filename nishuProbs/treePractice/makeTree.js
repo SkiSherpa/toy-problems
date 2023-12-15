5f408525499aa1146c3cc05c4a492dc090e33ddf
Erick Watanabe <erickjwatanabe@gmail.com>
2023-12-15 15:16:01 -0800

    finish make tree prob

--- nishuProbs/treePractice/makeTree.js
+++ nishuProbs/treePractice/makeTree.js
@@ -22,9 +22,20 @@ const root = new TreeNode(4);
 const Node2 = new TreeNode(2);
 const Node6 = new TreeNode(6);
 const Node5 = new TreeNode(5);
+const Node3 = new TreeNode(3);
+const Node1 = new TreeNode(9);
+const Node7 = new TreeNode(7);
+const Node9 = new TreeNode(9);
+const Node11 = new TreeNode(11);
+const Node12 = new TreeNode(12);
+const Node13 = new TreeNode(13);
 
 // connect nodes
 root.children.push(Node2, Node6, Node5);
+Node2.children.push(Node3, Node1);
+Node6.children.push(Node9);
+Node5.children.push(Node7);
+Node7.children.push(Node11, Node12, Node13);
 
 
 console.log(root);
