d379fd8117b3968596d1d018a2bb6cf29d9299b7
Erick Watanabe <erickjwatanabe@gmail.com>
2023-12-15 15:10:14 -0800

    understand basics

--- nishuProbs/treePractice/makeTree.js
+++ nishuProbs/treePractice/makeTree.js
@@ -8,3 +8,23 @@
        11 12 13
 
 */
+
+// make blueprint for nodes
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.children = [];
+    }
+}
+
+// make nodes
+const root = new TreeNode(4);
+const Node2 = new TreeNode(2);
+const Node6 = new TreeNode(6);
+const Node5 = new TreeNode(5);
+
+// connect nodes
+root.children.push(Node2, Node6, Node5);
+
+
+console.log(root);
