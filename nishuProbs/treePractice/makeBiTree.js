776d844f57fc5f2859f3be6cad981db4a00d49fa
Erick Watanabe <erickjwatanabe@gmail.com>
2023-12-15 14:58:04 -0800

    tree practice

--- nishuProbs/treePractice/makeBiTree.js
+++ nishuProbs/treePractice/makeBiTree.js
@@ -1,11 +1,3 @@
-class TreeNode {
-    constructor(value) {
-        this.value = value;
-        this.right = null;
-        this.left = null;
-    }
-}
-
 // make a binary tree that has the following structure:
 /*
     4
@@ -16,3 +8,30 @@ class TreeNode {
          \
           8
 */
+
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.right = null;
+        this.left = null;
+    }
+}
+
+// making nodes
+const root = new TreeNode(4);
+const Node2 = new TreeNode(2);
+const Node5 = new TreeNode(5);
+const Node3 = new TreeNode(3);
+const Node1 = new TreeNode(1);
+const Node7 = new TreeNode(7);
+const Node8 = new TreeNode(8);
+
+// connecting nodes
+root.left = Node2;
+root.right = Node5;
+Node2.left = Node3;
+Node2.right = Node1;
+Node5.right = Node7;
+Node7.right = Node8;
+
+console.log(root);
