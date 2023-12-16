7c8f37aa76d82a12d097e22fe03ec956a8fe7712
Erick Watanabe <erickjwatanabe@gmail.com>
2023-12-16 14:45:48 -0800

    solved mostChildren prob. Adding a new test case to check

--- nishuProbs/treePractice/mostChildren.js
+++ nishuProbs/treePractice/mostChildren.js
@@ -1,4 +1,74 @@
-// Using the tree from makeTree find the Node with the most childen.
+// Using the tree from makeTree find the Node with the most childen. If there are ties, return all of them.
 
 // makeTree Node with most children = root & 7
 // both have 3
+/*
+     4
+   / | \
+  2  6  5
+ / \  \  \
+3   1  9  7
+        / | \
+       11 12 13
+*/
+
+// make blueprint for nodes
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.children = [];
+    }
+}
+
+// make nodes
+const root = new TreeNode(4);
+const Node2 = new TreeNode(2);
+const Node6 = new TreeNode(6);
+const Node5 = new TreeNode(5);
+const Node3 = new TreeNode(3);
+const Node1 = new TreeNode(9);
+const Node7 = new TreeNode(7);
+const Node9 = new TreeNode(9);
+const Node11 = new TreeNode(11);
+const Node12 = new TreeNode(12);
+const Node13 = new TreeNode(13);
+
+// connect nodes
+root.children.push(Node2, Node6, Node5);
+Node2.children.push(Node3, Node1);
+Node6.children.push(Node9);
+Node5.children.push(Node7);
+Node7.children.push(Node11, Node12, Node13);
+
+// make an obj to hold nodes with the most children
+    // {num of max children: [node1, node2]}
+// create current max children var set to zero
+// IF the current node's children array length is greater than the current max
+    // set the current childrens array length to current max
+    // push current node into winners array
+// loop through current node's children
+    // recursive call for current child
+
+// return obj[curMax]
+
+function findMaxChildren(node, mostKids = {}, curMax = 0) {
+    if (node === null) {
+        return mostKids;
+    }
+
+    if (node.children.length > curMax) {
+        curMax = node.children.length
+        mostKids[node.children.length] = [node];
+    } else if (node.children.length === curMax) {
+        mostKids[curMax].push(node);
+    }
+
+    for (let i = 0; i < node.children.length; i++) {
+        let curChild = node.children[i];
+        findMaxChildren(curChild, mostKids, curMax);
+    }
+    return mostKids[curMax];
+
+}
+
+console.log(findMaxChildren(root));
---
+++ nishuProbs/treePractice/testMostkidsTree.js
@@ -0,0 +1,16 @@
+class tree {
+    constructor(val) {
+        this.val = val;
+        this.kids = [];
+    }
+}
+
+/*
+     1
+   /  \
+  2    3
+ / \    \
+4   5    6
+        / | \
+       7  8  9
+*/
