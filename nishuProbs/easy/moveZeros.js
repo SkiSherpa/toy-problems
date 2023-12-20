26c565fed63b225b2dc1deebfcccc5f2d275c32f
Erick Watanabe <erickjwatanabe@gmail.com>
2023-12-20 15:15:25 -0800

    understand double pointers

--- nishuProbs/easy/moveZeros.js
+++ nishuProbs/easy/moveZeros.js
@@ -21,6 +21,53 @@ Constraints:
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
+// IP: array of nums
+// OP: same array, but zeros moved to back
 
+// make a var to hold a count of how many zeros there are
+// loop through nums
+    // if the current is zero
+        // zeroCount++
+        // move array over by 1 with splice
+// loop by zeroCount
+    // add zeros to end of nums
+var _moveZeroes = function(nums) {
+    let numsLen = nums.length
+    for (let i = 0; i < numsLen; i++) {
+        let cur = nums[i];
+        console.log("cur: ", cur, "i", i);
+        if (cur === 0) {
+            let toAdd = nums.slice(i+1);
+            console.log("i", i, toAdd);
+            nums.splice(i, toAdd.length, ...toAdd);
+            console.log(nums);
+            nums.splice(nums.length - 1, 1, 0);
+            console.log("3---", nums);
+
+        }
+    }
+    return nums;
 };
+
+// another persons sol:
+var moveZeroes = function(nums) {
+    let i = 0;
+    let j = i + 1;
+
+    while (j <= nums.length - 1) {
+        if (nums[i] !== 0) {
+            i++;
+            j++;
+        } else {
+            // i is a zero
+            if (nums[j] !== 0) {
+                [nums[i], nums[j]] = [nums[j], nums[i]];
+                i++;
+            }
+            j++;
+        }
+        console.log(i, j, nums);
+    }
+}
+console.log(moveZeroes([0,1,0,3,12])); //[1,3,12,0,0]
+// console.log(moveZeroes([0, 0, 1])); // [1, 0, 0]
