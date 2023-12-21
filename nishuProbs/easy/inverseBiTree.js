83613a5a76c1963934ac79ebeb178626acf00bc6
Erick Watanabe <erickjwatanabe@gmail.com>
2023-12-21 13:34:22 -0800

    inverseTree added, with my sol + another's sol, & big O analysis

---
+++ nishuProbs/easy/inverseBiTree.js
@@ -0,0 +1,86 @@
+/*
+Given the root of a binary tree, invert the tree, and return its root.
+
+Example 1:
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+      4
+     / \
+    2   7
+   / \ / \
+  1  3 6  9
+      4
+     / \
+    2   7
+   / \ / \
+  1  3 6  9
+
+
+Example 2:
+Input: root = [2,1,3]
+Output: [2,3,1]
+   2         2
+  / \   ->  / \
+ 1   3     3   1
+
+Example 3:
+Input: root = []
+Output: []
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+*/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (!root) {
+        return root;
+    }
+    let left = root.left;
+    let right = root.right;
+
+    root.left = right;
+    root.right = left;
+
+    invertTree(root.left)
+    invertTree(root.right);
+
+    return root;
+};
+
+/*
+t = O(1)  |  m = O(1)
+t = 57ms, 26.52%
+m = 42.71MB, 6.5%
+*/
+
+// this is so similar but, faster
+// t = 50ms, 67.90% | m = 42.45MB, 24.53%
+let _invertTree = function(root) {
+    // Base case...
+    if(root == null){
+        return root
+    }
+    // Call the function recursively for the left subtree...
+    invertTree(root.left)
+    // Call the function recursively for the right subtree...
+    invertTree(root.right)
+    // swapping process...
+    const curr = root.left
+    root.left = root.right
+    root.right = curr
+    return root         // Return the root...
+}
