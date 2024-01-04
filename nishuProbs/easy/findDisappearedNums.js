71a224652911628c248ee271ea81d244d38c3ba5
Erick Watanabe <erickjwatanabe@gmail.com>
2024-01-04 13:07:03 -0800

    passing smallnums cases

--- nishuProbs/easy/findDisappearedNums.js
+++ nishuProbs/easy/findDisappearedNums.js
@@ -29,8 +29,38 @@ Follow up: Could you do it without extra space and in O(n) runtime? You may assu
 // if [4,3,2,7,8,2,3,1] -> 1 - 8 and 5 & 6 are not present
 // IP: array of nums
 // OP: array of nums - but missing nums from 1 - n, where n is the length of the IP nums array
+// C: Can there be ints out side the range of 1 to n contained in nums?
+// E: IF "nums" is empty, return empty arr
 
-//
+// what are the missing numbers from 1 - (len of "nums")
+// create an empty array "arr"
+// make "n" var equal to length of "nums"
+// create a loop going from 1, to "n"
+    // where i is the current number in range
+    // loop through "nums", use j
+        // IF i = the current number in nums
+            // break inner loop
+        // IF j is the last number
+            // add i to "arr"
+
+// return arr
 var findDisappearedNumbers = function(nums) {
-
+    if (nums.length === 0) {
+        return [];
+    }
+    let arr = [];
+    let n = nums.length;
+    for (let i = 1; i <= n; i++) {
+        // i should go from 1 - 8
+        for (let j = 0; j <= n; j++) {
+            if (i === nums[j]) {
+                break;
+            }
+            // console.log("i", i, "nums[j]", nums[j]);
+            if (j === n) {
+                arr.push(i);
+            }
+        }
+    }
+    return arr;
 };
