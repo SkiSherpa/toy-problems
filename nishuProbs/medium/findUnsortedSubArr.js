942d6f1b678692c392210f35c6b50316b0772395
Erick Watanabe <erickjwatanabe@gmail.com>
2024-01-06 14:40:10 -0800

    working sol with understanding, big o analysis

--- nishuProbs/medium/findUnsortedSubArr.js
+++ nishuProbs/medium/findUnsortedSubArr.js
@@ -11,7 +11,7 @@ Example 1:
 Input: nums = [2,6,4,8,10,9,15]
 Output: 5
 Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
-[4, 6, 8, 9, 10] -> [1,4,6,8,9,10,15]
+[4, 6, 8, 9, 10] -> [2,4,6,8,9,10,15]
 
 Example 2:
 Input: nums = [1,2,3,4]
@@ -34,6 +34,44 @@ Follow up: Can you solve it in O(n) time complexity?
  * @param {number[]} nums
  * @return {number}
  */
+
+// make a copy of nums
+// sort the copy
+// use pincer to compare when numbers are different
+// loop through arr's - start of arr's
+// IF cur of Nums !== cur of Copy
+    // break - save i
+// loop through back of arrs
+// IF cur of Nums !== cur of Copy
+    // break - save j
+// return end - start + 1
+var findUnsortedSubarray = function(nums) {
+    let copy = nums.slice().sort((a, b) => a - b);
+    let i = 0;
+    let j = nums.length - 1;
+
+    while (i <= j) {
+        if (nums[i + 1] === undefined) {
+            return 0;
+        }
+        if (nums[i] !== copy[i]) {
+            break;
+        }
+        i++;
+    }
+
+    while (j >= 0) {
+        if (nums[j] !== copy[j]) {
+            break;
+        }
+        j--;
+    }
+    return j - i + 1;
+}
+// t = O(nlog(n)), where n is the number of numbers.
+    // I think nlog(n) from the sort, the two whiles together is ~ n. = nlog(n) + n
+    // If nums is sorted, then first while will go through the whole list
+
 // a pincer
 // start = 0
 // end = arr.length - 1 (last element)
@@ -77,58 +115,97 @@ var _findUnsortedSubarray = function(nums) {
         start++;
     }
     // console.log("bnefore isSame, end", end);
-    let isSame = end;
+    // let isSame = end;
     // console.log("isSame", isSame);
     while (end >= start) {
         // console.log("in end loop, start, end", start, end);
         if (nums[end - 1] > nums[end]) {
-            end = isSame;
+            // end = isSame;
             break;
         }
-        if (nums[end - 1] < nums[end]) {
-            // set end to isSame
-            break;
-        }
+        // if (nums[end - 1] < nums[end]) {
+        //     // set end to isSame
+        //     continue;
+        // }
         end--
     }
-    // console.log("start, end", start, end, "isSame", isSame) // 1,4
-    let sub = nums.slice(start, end+1);
-    // console.log(sub);
-    return sub.length;
+    console.log("start, end", start, end) // 1,4
+    return end - start + 1;
 };
 
-
-var findUnsortedSubarray = function(nums) {
+// consfusing sol below, not mine
+var __findUnsortedSubarray = function(nums) {
     let len = nums.length - 1;
     let left = -1;
     let right = -1;
     let  max = nums[0];
     let min = nums[len];
-
+    console.log("i |", "left", "a", "max", "right", "b", "min");
     for (let i = 1; i <= len; i++) {
         let a = nums[i]
         let b = nums[len-i];
-        console.log("a < max", a, "<", max);
-        console.log("b > min", b, ">", min);
+        console.log(i,"|", left," ", a , " ", max, " ", right, " ", b, min);
         a < max ? right = i : max = a;
         b > min ? left = i : min = b;
-        console.log("max, min", max, min, "r", right, "L", left); // max = a = 6, min = b = 9
+        console.log(i,"|", left," ", a , " ", max, " ", right, " ", b, min);
         console.log("======");
     }
-    console.log(left,"+", right,"-", len, "+1");
+    console.log("L", left, "R", right, len + 1)
     return Math.max(0, left + right - len + 1)
 }
 
+// make a copy of nums
+// sort the copy
+// use pincer to compare when numbers are different
+// loop through arr's - start of arr's
+// IF cur of Nums !== cur of Copy
+    // break - save i
+// loop through back of arrs
+// IF cur of Nums !== cur of Copy
+    // break - save j
+// return end - start + 1
+var findUnsortedSubarray = function(nums) {
+    let copy = nums.slice().sort((a, b) => a - b);
+    let i = 0;
+    let j = nums.length - 1;
+
+    while (i <= j) {
+        if (nums[i + 1] === undefined) {
+            return 0;
+        }
+        if (nums[i] !== copy[i]) {
+            break;
+        }
+        i++;
+    }
+
+    while (j >= 0) {
+        if (nums[j] !== copy[j]) {
+            break;
+        }
+        j--;
+    }
+    return j - i + 1;
+}
+// I don't understand left and right, why use the formula that is there?
+// I think he's counting the two ends around the sub array... I dont think that's the case any more.
+
 // console.log(findUnsortedSubarray([2,6,4,8,10,9,15])) // 5
-// console.log(findUnsortedSubarray([1,2,3,4])) // 0
+// console.log(findUnsortedSubarray([1,2,3,4])); // 0
+// console.log(findUnsortedSubarray([1,3,2,4])); // 2
+// console.log(findUnsortedSubarray([1,1,2,3,4]));// 0
 // console.log(findUnsortedSubarray([2])) // 0
 // console.log(findUnsortedSubarray([2,6])) // 0
-// console.log(findUnsortedSubarray([2,6,4])) // 2
+// console.log(findUnsortedSubarray([1,3,2])) // 2
+// why set left to 1 on first pass?
+
 // console.log(findUnsortedSubarray([2,2,2,2,2]));// 0
 // // // ---
 // console.log(findUnsortedSubarray([1,2, 3, 3, 3])) // 0
 // These two are contradicting each other in my code, might need a new approach.
 // console.log("ans", findUnsortedSubarray([1,3,2,2,2])) // 4
 // below is the odd case where the prev is smaller
-console.log("ans", findUnsortedSubarray([1,3,2,3,3])) // 2
+// console.log("ans", findUnsortedSubarray([1,3,2,3,3])) // 2
 // ---
+// [1,4,2,2,3]
+console.log(findUnsortedSubarray([1,4,2,2,3]));// 4
