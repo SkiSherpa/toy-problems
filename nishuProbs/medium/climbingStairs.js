8f2cc62f397cfe57c6b8a1b31320199030502653
Erick Watanabe <erickjwatanabe@gmail.com>
2024-02-02 11:32:29 -0800

    climbing stairs by 1 or 2

---
+++ nishuProbs/medium/climbingStairs.js
@@ -0,0 +1,76 @@
+/* climbing stairs
+Each time you can climb 1 or 2 steps
+How many distinct ways can you climb to the top?
+ex: 5 stairs
+1 1 1 1 1
+
+1 1 1 2
+1 1 2 1
+1 2 1 1
+2 1 1 1
+
+1 2 2
+2 1 2
+2 2 1
+= 8
+[1, 4, 3]
+
+ex: 4 stairs
+1 1 1 1
+1 1 2
+1 2 1
+2 1 1
+2 2
+= 5 ways
+[1, 3, 1]
+
+ex: 3 stairs
+1 1 1
+1 2
+2 1
+= 3
+*/
+/*
+IF odd number of tot stairs
+    count the 1 for all 1 steps
+    count n-1 set of square matricies - 5 stairs, you'll have 4 rows of a square matrix
+    count n-2 set of
+    ----
+    11111 -> x1
+    1112 -> x4
+    112 -> x3
+    -----
+    111 -> x1
+    12 -> 2
+    ----- 7 stairs
+    1111111 -> x1
+    111112 -> x6
+    11122 -> x5
+    1222 -> x4
+IF even number
+    count
+*/
+/* recursion - think about it as walking down the stairs
+
+starting a n
+you can do either n-1 or n-2
+
+base case(n) - IF n === 0, return 1
+ IF n < 0, return 0;
+
+return countSteps(n-1) + countSteps(n-2)
+*/
+let countSteps = function(n) {
+    if (n === 0) {
+        return 1;
+    }
+    if (n < 0) {
+        return 0;
+    }
+    return countSteps(n - 1) + countSteps(n - 2);
+}
+
+console.log(countSteps(5), "ans: 8");
+// how to modify this to do in linear time
+// hint: memoization
+// right now its t = O(2^n) - real bad
