1a838d7f275d8229ec61943a8d645bdea2944280
Erick Watanabe <erickjwatanabe@gmail.com>
2024-02-11 11:36:11 -0800

    maxArea solved, with analysis

--- nishuProbs/medium/maxArea.js
+++ nishuProbs/medium/maxArea.js
@@ -36,7 +36,6 @@ n == height.length
 
 // computing which has the largest base*curHeight
 
-
 // let len = height.length - 1;
 // let largest = 0;
 // create start = 0 and end = 1 pointers
@@ -52,7 +51,7 @@ n == height.length
             // largest = cur area
         // end++
     // start++
-    var maxArea = function(height) {
+    var _maxArea = function(height) {
         let len = height.length - 1;
         let largest = 0;
         let start = 0;
@@ -71,3 +70,40 @@ n == height.length
         }
         return largest;
     };
+// The O(n^2) is causing it to time out when IP array is large
+/*
+Instead of starting end at start - 1, end = heights.length - 1
+Move either pointer up or down by one, respect
+calc the area
+IF current area is longer than the longest
+    re-assign largest area
+*/
+var maxArea = function(height) {
+    let len = height.length - 1;
+    let largestArea = 0;
+    let start = 0;
+    let end = len;
+    while (start < end) {
+        let curH = 0;
+        // find current x-axis length
+        let curLen = end - start;
+        // find lowest of two pointers
+            // set that to curH
+            // ++/-- depeneding on which is set
+        if (height[start] < height[end]) {
+            curH = height[start];
+            start++;
+        } else {
+            curH = height[end];
+            end--;
+        }
+        let curArea = curLen*curH;
+        if (largestArea < curArea) {
+            largestArea = curArea
+        }
+    }
+    return largestArea;
+};
+//  t = O(n), where n is the length of height;
+// m = O(1), only vars are saved
+// t = 60ms 87.18%, m = 56.94 26.22%
