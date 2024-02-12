2d128b39e801c1821f77e72ef6f1429df5dc530c
Erick Watanabe <erickjwatanabe@gmail.com>
2024-02-12 10:12:53 -0800

    added notes for understanding

--- nishuProbs/medium/generateParentheses.js
+++ nishuProbs/medium/generateParentheses.js
@@ -7,9 +7,15 @@ Output: ["((()))","(()())","(())()","()(())","()()()"]
 
 Example 2:
 Input: n = 1
-Output: ["()"]
+Output: ["()"] - invalid -> )(
 
+Ex3:
+IP: n = 2
+OP: ["()()", "(())"] - invalid -> )()(, ))((, )()(
 
+Ex4:
+IP: n = 4
+OP: [ (((()))), (()()()), (())(()), ((()))(), ()((()))...]
 Constraints:
 
 1 <= n <= 8
@@ -18,6 +24,11 @@ Constraints:
  * @param {number} n
  * @return {string[]}
  */
+// IP: a number - 1 <= n <= 8
+// OP: an array of strings - generated valid parentheses
+
+// Use a stack structure to determine if valid set of parenths
+
 var generateParenthesis = function(n) {
 
 };
