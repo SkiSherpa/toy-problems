675008e7fd7ac3d0126234574d2f14afade9302e
Erick Watanabe <erickjwatanabe@gmail.com>
2024-02-15 13:37:41 -0800

    findDuplicate added, solved, and analysis

---
+++ nishuProbs/medium/findDuplicate.js
@@ -0,0 +1,47 @@
+/**
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+Example 1:
+Input: nums = [1,3,4,2,2]
+Output: 2
+
+Example 2:
+Input: nums = [3,1,3,4,2]
+Output: 3
+
+Constraints:
+
+1 <= n <= 10^5
+nums.length == n + 1
+1 <= nums[i] <= n
+All the integers in nums appear only once except for precisely one integer which appears two or more times.
+ * @param {number[]} nums
+ * @return {number}
+ */
+// IP: an array of nums
+// OP: a number - the single repeating number
+// C: cannot modify nums
+// C: only m = O(1); - can't make a new array
+
+// create a set
+// loop through nums
+    // IF current number is in the set
+        // return that number
+    // otherwise add that number to set
+var findDuplicate = function(nums) {
+    let found = new Set();
+    for (let i = 0; i < nums.length; i++) {
+        if (found.has(nums[i])) {
+            return nums[i];
+        } else {
+            found.add(nums[i]);
+        }
+    }
+};
+// t = O(n), n is length of nums
+// m = O(n), n is length of nums,
+// it passes all the tests and answer is accepted
--- nishuProbs/medium/lengthOfLIS.js
+++ nishuProbs/medium/lengthOfLIS.js
@@ -1,35 +1,65 @@
 /**
 Given an integer array nums, return the length of the longest strictly increasing
 subsequence
-.
-
-
 
 Example 1:
-
 Input: nums = [10,9,2,5,3,7,101,18]
 Output: 4
 Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
-Example 2:
 
+Example 2:
 Input: nums = [0,1,0,3,2,3]
 Output: 4
-Example 3:
 
+Example 3:
 Input: nums = [7,7,7,7,7,7,7]
 Output: 1
 
-
 Constraints:
-
 1 <= nums.length <= 2500
--104 <= nums[i] <= 104
-
+-10^4 <= nums[i] <= 10^4
 
 Follow up: Can you come up with an algorithm that runs in O(n log(n)) time complexity?
  * @param {number[]} nums
  * @return {number}
  */
-var lengthOfLIS = function(nums) {
+/*
+Strictly increasing means, increasing in value, with no repeats.
+It doesn't have to consectutive in the original sequence to count, why in ex1 you can skip 5
+*/
+// IP: array of nums
+// OP: a number - longest strictly increasing subsequence
 
+// create a count set to zero
+// create two pointers, but one starts +1 to other
+// loop through nums
+    // if num at i < nums at j
+        // make count go up by one
+    // otherwise, equal or greater than
+        // move j up by one
+    // i++ and j++
+var _lengthOfLIS = function(nums) {
+    let i = 0;
+    let j = 1;
+    let count = 0;
+    let startCount = false
+    while (j < nums.length) {
+        console.log(i, j, nums[i], nums[j])
+        if (nums[i] < nums[j]) {
+            startCount = true;
+            count++;
+        } else if (startCount) {
+            j++;
+        }
+        i++;
+        j++;
+    }
+    return count;
 };
+
+
+// didn't get the first one to pass,
+// I did spend 15 mins on figuring out what strictly increasing means,
+// and why ex1 skips 5.
+
+console.log(lengthOfLIS([10,9,2,5,3,7,101,18]), [2,3,7,101], "ans is ", 4);
