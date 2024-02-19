c05b8952294d92dd7d209c7d4d7f9f7d565af2aa
Erick Watanabe <erickjwatanabe@gmail.com>
2024-02-19 10:36:40 -0800

    sol for squareIsWhite and refactored sol with analysis

--- nishuProbs/easy/squareWithWhite.js
+++ nishuProbs/easy/squareWithWhite.js
@@ -2,6 +2,14 @@
  * https://leetcode.com/problems/determine-color-of-a-chessboard-square/description/
 You are given coordinates, a string that represents the coordinates of a square of the chessboard. Below is a chessboard for your reference.
 
+[a8,b8,c8,d8,e8,f8,g8,h8]
+[a7,b7,c7,d7,e7,f7,g7,h7]
+...
+[a1,b1,c1,d1,e1,f1,g1,h1]
+
+a1 = black
+a8 = white
+
 Return true if the square is white, and false if the square is black.
 
 The coordinate will always represent a valid chessboard square. The coordinate will always have the letter first, and the number second.
@@ -27,6 +35,53 @@ coordinates.length == 2
  * @param {string} coordinates
  * @return {boolean}
  */
-var squareIsWhite = function(coordinates) {
+// IP: a string - "a-h & 1-8": ex: a1, 3g, 9h
+// OP: a bool - true if coor are white, false if black
 
+// the a, c, e, g col,
+    // IF odd row
+        // its black
+    // otherwise, its white
+// the other col, b,d,f,h
+    // IF odd row
+        // its white
+    // optherwise its black
+var _squareIsWhite = function(coordinates) {
+    if (coordinates[0] === 'a' || coordinates[0] === 'c' ||coordinates[0] === 'e' || coordinates[0] === 'g') {
+        if (coordinates[1] % 2 === 1) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        if (coordinates[1] % 2 === 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
+
+// t = O(1), all if checks with no loops or search algos
+// m = O(1)
+// t = 55ms 37.95% | m = 48.70MB 28.13%
+
+var squareIsWhite = function(coordinates) {
+    if (/[aceg]/.test(coordinates[0])) {
+        if (coordinates[1] % 2 === 1) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        if (coordinates[1] % 2 === 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+// The regex is a little faster, than the series of if statements
+// t = 43ms 91.96% | m = 48.67 MB, 37.95%
+console.log(squareIsWhite('a1'), false);
+console.log(squareIsWhite('b5'), true);
