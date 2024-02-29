a459f1721e82610460c8023a76afd976b8d21c33
Erick Watanabe <erickjwatanabe@gmail.com>
2024-02-29 12:41:45 -0800

    maxProdcut added

--- nishuProbs/easy/addBinary.js
+++ nishuProbs/easy/addBinary.js
@@ -49,7 +49,7 @@ a, b = 0, result is zero
 loop through a b backwards
 IF a or b is undefined, take the other number and add to result string
 */
-var addBinary = function(a, b) {
+var _addBinary = function(a, b) {
     let result = '';
     let len = a.length > b.length ? a.length - 1 : b.length - 1;
     let sums = [];
@@ -86,11 +86,66 @@ var addBinary = function(a, b) {
     }
     return result;
 };
+// RAN out of time, first approach I hit an issue and came up with the one above
+// below is a different persons approach
+var addBinary = function (a, b) {
+    let num1Arr = a.split("");
+    let num2Arr = b.split("");
+    let result = [];
+
+    let temp = 0;
+
+    let j = num2Arr.length - 1;
+    let i = num1Arr.length - 1;
+    console.log(num1Arr, num2Arr);
+    while (i >= 0 || j >= 0 || temp > 0) {
+
+        let value = (i >= 0 ? Number(num1Arr[i]) : 0) + (j >= 0 ? Number(num2Arr[j]) : 0) + temp;
+
+        console.log(i, j, "vals from arrs 1, then 2.", num1Arr[i], num2Arr[j], value, 'temp,', temp);
+
+        if (value === 2) {
+            result.push(0);
+            temp = 1;
+        }
+        else if (value === 1) {
+            result.push(1);
+            temp = 0;
+        }
+        else if (value === 0) {
+            result.push(0);
+            temp = 0;
+        }
+        else if (value === 3) {
+            result.push(1);
+            temp = 1;
+        }
+        i--;
+        j--;
+        console.log("res", result);
+    }
+
+    if (temp > 0) {
+        result.push(temp);
+    }
+    console.log(result);
+    return result.reverse().join('');
+};
 // Example 1:
 // Input: a = "11", b = "1"
 // Output: "100"
-console.log(addBinary('11', '1'), '100');
+// console.log(addBinary('11', '1'), '100');
 // Example 2:
 // Input: a = "1010", b = "1011"
 // Output: "10101"
 // console.log(addBinary('1010', '1011'), '10101');
+//  1010
+// 11100
+//100110
+console.log(addBinary('1010', '11100'), '100110');
+
+// split IP arrs on empty space
+// create carry var = 0
+// get lengths of both IP arrs
+// loop backwards, kill if i OR j >=0  OR (also carry >=0)... not sure why
+    // value = Number at arrs being greater than zero == the number at array.  + carry val
