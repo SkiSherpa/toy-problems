597613938c6076f777cf17a96dbc26687d98df37
Erick Watanabe <erickjwatanabe@gmail.com>
2024-03-06 11:38:59 -0800

    attempted

--- nishuProbs/easy/pascalTriangle2.js
+++ nishuProbs/easy/pascalTriangle2.js
@@ -1,5 +1,5 @@
 /**
- *
+ * https://leetcode.com/problems/pascals-triangle-ii/description/
 Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
 
 In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
@@ -24,6 +24,43 @@ Constraints:
  * @param {number} rowIndex
  * @return {number[]}
  */
-var getRow = function(rowIndex) {
+// IP: a number - target rowIndex
+// OP: array of nums - values of the target rowIndex
 
+// 0 = [1]
+// 1 = [1,1]
+// save previous row
+// use prev row to calc next row
+
+
+var getRow = function(rowIndex) {
+    if (rowIndex === 0) {
+        return [1];
+    }
+    if (rowIndex === 1) {
+        return [1,1];
+    }
+
+    let row = [1,1];
+    // create a new row that is one length longer than argu row
+    // [1, ..., 1]
+    // take prev row
+    // formula to calc which i from argue row
+    // loop to cal next values
+        // push into arr
+
+
+    function nextRow(prevRow) {
+        let arr = new Array(prevRow.length + 1);
+        arr[0] = 1;
+        arr[arr.length - 1] = 1;
+        for (let i = 1; i < arr.length; i++) {
+            let prevIndex = i - 0;
+            arr[i] = prevRow[i] + prevRow[prevIndex];
+        }
+        return arr;
+    }
+    return nextRow(row);
 };
+
+console.log(getRow(4));
