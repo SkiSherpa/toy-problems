f2666ecacdbc434c47c09b73043f5b79aaef84cd
Erick Watanabe <erickjwatanabe@gmail.com>
2024-03-10 13:32:30 -0700

    added test cases for 7-11

--- nishuProbs/easy/indexWorksheet.js
+++ nishuProbs/easy/indexWorksheet.js
@@ -38,13 +38,13 @@ const t = [
 ]
 
 
-// * What is the item in the 2nd row and 3rd column ?
+// 1 What is the item in the 2nd row and 3rd column ?
     // console.log(t[1][2], 5);
 
-// * What is the item in the 3rd column and 2nd row?
+// 2 What is the item in the 3rd column and 2nd row?
     // console.log(t[1][2], 5);
 
-// * What is the sum of all values in the matrix?
+// 3 What is the sum of all values in the matrix?
     // loop through t
         // loop through each subarray
             // add each item to sum
@@ -59,7 +59,7 @@ const t = [
     }
     // console.log(sum(t), 0+1+2+3+4+5+6+7+8+9+1);
 
-// * What is the sum of all values in the 1st column?
+// 4 What is the sum of all values in the 1st column?
     // loop through t
         // add first item to sum
     let firstColSum = (t) => {
@@ -71,7 +71,7 @@ const t = [
     }
     // console.log(firstColSum(t), 0+3+6+9);
 
-// * What is the sum of all values in the 3rd row?
+// 5 What is the sum of all values in the 3rd row?
     // loop through 3rd row
         // add each item to sum
     let thirdRowSum = (t) => {
@@ -83,7 +83,7 @@ const t = [
         return sum;
     }
     // console.log(thirdRowSum(t), 6+7+8);
-// * Verify that the input is in fact an nxm matrix of numbers and report what n and m are, n=4, m=3
+// 6 Verify that the input is in fact an nxm matrix of numbers and report what n and m are, n=4, m=3
     // n = num of items in each rows,
     // m = num of rows
     // to verify would be same number of items in each row
@@ -151,12 +151,42 @@ const t = [
     // console.log(isNxM(notNxM), false);
     // console.log(isNxM(notNxMObj), false);
 
-// * Are there any negative values in the 2nd row?
-// * What are the sum of all values in the topmost row?
-// * What is the product of all values in the bottommost row?
-// * What is the sum of all values in the rightmost column?
-// * What is the product of all values in the leftmost column?
-// * Are there any positive values along the diagonal from top left to bottom right? (If n and m are not the same, then pretend that the larger of the two is equal to the smaller of the two, ignoring the later row(s) or column(s))
-// * What is the sum of all values along the diagonal from the top right to bottom left?
-// * Which row has the highest product of all its values?
-// * Which column has the highest product of all its values?
+// 7 Are there any negative values in the 2nd row?
+    const yesNeg = [
+        [0,1,2],
+        [3,4,-5],
+        [6,7,8],
+        [9,0,1]
+    ]
+    const isNegIn2nd = (t) => {
+
+    }
+    console.log(isNegIn2nd(t), false);
+    console.log(isNegIn2nd(yesNeg), true);
+// 8 What are the sum of all values in the topmost row?
+    const topRowSum = (t) => {
+
+    }
+    console.log(topRowSum(t), 3);
+// 9 What is the product of all values in the bottommost row?
+    const bottomRowProduct = () => {
+
+    }
+    console.log(bottomRowProduct(t), 0);
+// 10 What is the sum of all values in the rightmost column?
+    const rightColSum = (t) => {
+
+    }
+    console.log(rightColSum(t), 2+5+8+1);
+// 11 What is the product of all values in the leftmost column?
+    const leftColSum = (t) => {
+
+    }
+    console.log(leftColSum(t), 0+3+6+9);
+// 12 Are there any positive values along the diagonal from top left to bottom right? (If n and m are not the same, then pretend that the larger of the two is equal to the smaller of the two, ignoring the later row(s) or column(s))
+
+// 13 What is the sum of all values along the diagonal from the top right to bottom left?
+
+// 14 Which row has the highest product of all its values?
+
+// 15 Which column has the highest product of all its values?
