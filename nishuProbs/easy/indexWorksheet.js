f28343365b182a1846ae586678acad90e08dce6e
Erick Watanabe <erickjwatanabe@gmail.com>
2024-03-10 12:34:18 -0700

    solved 1-5

--- nishuProbs/easy/indexWorksheet.js
+++ nishuProbs/easy/indexWorksheet.js
@@ -36,28 +36,52 @@ const t = [
     [6,7,8],
     [9,0,1]
 ]
-// sum all nums in matrix
-// loop through t
-    // loop through each subarray
-        // add each item to sum
-let sum = (t) => {
-    let total = 0;
-    for (let i = 0; i < t.length; i++) {
-        for (let j = 0; j < t[i].length; j++) {
-            total += t[i][j];
-        }
-    }
-    return total;
-}
+
+
 // * What is the item in the 2nd row and 3rd column ?
-    console.log(t[1][2], 5);
+    // console.log(t[1][2], 5);
+
 // * What is the item in the 3rd column and 2nd row?
-    console.log(t[1][2], 5);
+    // console.log(t[1][2], 5);
+
 // * What is the sum of all values in the matrix?
-    console.log(sum(t), 0+1+2+3+4+5+6+7+8+9+1);
+    // loop through t
+        // loop through each subarray
+            // add each item to sum
+    let sum = (t) => {
+        let total = 0;
+        for (let i = 0; i < t.length; i++) {
+            for (let j = 0; j < t[i].length; j++) {
+                total += t[i][j];
+            }
+        }
+        return total;
+    }
+    // console.log(sum(t), 0+1+2+3+4+5+6+7+8+9+1);
+
 // * What is the sum of all values in the 1st column?
-    console.log(firstColSum(t), 0+3+6+9);
+    // loop through t
+        // add first item to sum
+    let firstColSum = (t) => {
+        let sum = 0;
+        for (let i = 0; i < t.length; i++) {
+            sum += t[i][0];
+        }
+        return sum;
+    }
+    // console.log(firstColSum(t), 0+3+6+9);
+
 // * What is the sum of all values in the 3rd row?
+    // loop through 3rd row
+        // add each item to sum
+    let thirdRowSum = (t) => {
+        let sum = 0;
+        let thirdRow = t[2];
+        for (let i = 0; i < thirdRow.length; i++) {
+            sum += thirdRow[i];
+        }
+        return sum;
+    }
     console.log(thirdRowSum(t), 6+7+8);
 // * Verify that the input is in fact an nxm matrix of numbers and report what n and m are, n=4, m=3
 
