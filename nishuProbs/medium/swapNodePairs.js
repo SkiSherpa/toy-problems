55f3e9ffa95be84ec7fb81848ecb2f73f795e672
Erick Watanabe <erickjwatanabe@gmail.com>
2024-03-29 10:33:26 -0700

    problem added

--- nishuProbs/medium/swapNodePairs.js
+++ nishuProbs/medium/swapNodePairs.js
@@ -36,48 +36,67 @@ The number of nodes in the list is in the range [0, 100].
 
 // loop through the list counting by 2
     // switch the current and the next node
-    var swapPairs = function(head) {
-        if (!head || !head.next) {
-            return head;
+var swapPairs = function(head) {
+    if (!head || !head.next) {
+        return head;
+    }
+    let dummyHead = new ListNode(null);
+    // create a fn to add the next two nodes to dummyHead
+    function addNextTwo(lastNode, current, nextNode) {
+        console.log(lastNode, current, nextNode);
+        if (!current && !nextNode) {
+            return dummyHead.next;
         }
-        let dummyHead = new ListNode(null);
-        // create a fn to add the next two nodes to dummyHead
-        function addNextTwo(lastNode, current, nextNode) {
-            console.log(lastNode, current, nextNode);
-            if (!current && !nextNode) {
-                return dummyHead.next;
-            }
-            if (!nextNode) {
-                lastNode.next = current;
-                return dummyHead.next;
-            }
-            // set up the next node for recursive call
-            let nextCurrent;
-            let nextNextCur;
-            // IF on last node
-                // nextNode.next === undefined, we want to retrun dummy.next
-            if (!nextNode.next) {
-                return dummyHead.next;
-            } else {
-                nextCurrent = nextNode.next;
-            }
-            // adding next node to last node in dummyHead
-            lastNode.next = nextNode;
-
-            // IF odd head length, then next.next === undefined
-            // need to attach next Node before here
-            if (!nextNode.next.next) {
-                return dummyHead.next;
-            } else {
-                nextNextCur = nextNode.next.next;
-            }
-
-            // add secondNode to "nextNode"
-            nextNode.next = current;
-
-            return addNextTwo(current, nextCurrent, nextNextCur);
+        if (!nextNode) {
+            lastNode.next = current;
+            return dummyHead.next;
         }
-        let curHead = head;
-        let nextNodeHead = head.next;
-        return addNextTwo(dummyHead, curHead, nextNodeHead);
-    };
+        // set up the next node for recursive call
+        let nextCurrent;
+        let nextNextCur;
+        // IF on last node
+            // nextNode.next === undefined, we want to retrun dummy.next
+        if (!nextNode.next) {
+            return dummyHead.next;
+        } else {
+            nextCurrent = nextNode.next;
+        }
+        // adding next node to last node in dummyHead
+        lastNode.next = nextNode;
+
+        // IF odd head length, then next.next === undefined
+        // need to attach next Node before here
+        if (!nextNode.next.next) {
+            return dummyHead.next;
+        } else {
+            nextNextCur = nextNode.next.next;
+        }
+
+        // add secondNode to "nextNode"
+        nextNode.next = current;
+
+        return addNextTwo(current, nextCurrent, nextNextCur);
+    }
+    let curHead = head;
+    let nextNodeHead = head.next;
+    return addNextTwo(dummyHead, curHead, nextNodeHead);
+};
+
+
+    class ListNode {
+        constructor(val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    const head1 = new ListNode(1);
+    const node12 = new ListNode(2);
+    const node13 = new ListNode(3);
+    const node14 = new ListNode(4);
+
+    head1.next = node12;
+    node12.next = node13;
+    node13.next = node14;
+
+    console.log(swapPairs(head1), [2,1,4,3]);
