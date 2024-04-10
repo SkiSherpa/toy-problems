d71850126d37dc7f430843b4e26892ab7dc72327
Erick Watanabe <erickjwatanabe@gmail.com>
2024-04-10 13:53:29 -0700

    containsDupilcates2 solved with analysis

--- nishuProbs/easy/containsDuplicate2.js
+++ nishuProbs/easy/containsDuplicate2.js
@@ -31,6 +31,123 @@ Constraints:
 // IP: array of nums, a int
 // OP: Bool - true if 3 conditions met
 
+// video's sol
+// !!! the big conceptual take away is - as long as there is 1 duplicate in nums its true
+
+// creates a set
+// set i = 0
+// loop through nums with j
+    // IF j - i > k
+        // remove nums[i] from the set
+        // i++
+    // IF nums[j] is in the set
+        // return true - as long as there is 1 duplicate in nums its true
+    // add nums[j] to the set
+// return false
+var containsNearbyDuplicate = function(nums, k) {
+    console.log('==============')
+    let window = new Set();
+    let i = 0;
+    for (let j = 0; j < nums.length; j++) {
+        console.log(i, j, 'cur i', nums[i], 'sur j', nums[j])
+        console.log(window);
+        if (j - i > k) {
+            console.log('will delete', nums[i]);
+            // since nums[j] is always added, IF out of bounds for k
+            // you need to remove the last one added, b/c its out of bounds
+            window.delete(nums[i]);
+            i++;
+        }
+        if (window.has(nums[j])) {
+            return true;
+        }
+        window.add(nums[j]);
+    }
+    return false;
+}
+// t = O(kn), where n is the length of nums, k is a constant
+// m = O(n), where n is the length of nums,
+    // you have the case where there are no dups and you've added every number to the set
+
+// create an obj {number: bool - true if any of numbers pass}
+// loop through nums
+    // IF current is NOT in obj
+        // loop through to k away from current
+            // IF j current equals i current
+                // add current number to obj, with val of true
+// create arr of obj values
+// IF length of arr is NOT zero
+    // return true
+// return false
+var ___containsNearbyDuplicate = function(nums, k) {
+    let obj = {};
+    for (let i = 0; i < nums.length; i++) {
+        let cur = nums[i];
+        if (!obj[cur]) {
+            for (let j = i+1; j < k+i+1; j++) {
+                if (cur === nums[j]) {
+                    obj[cur] = true;
+                }
+            }
+        }
+    }
+    let bools = Object.values(obj);
+    if (bools.length > 0) {
+        return true;
+    }
+    return false;
+}
+
+// create an obj {number: bool - default is false, true if any of numbers pass}
+// loop through nums
+    // loop through rest of nums, where j starts at i + 1
+        // IF nums at i and j are equal
+            // IF current number is in obj
+                // IF |i - j| <= k
+                    // change obj[num] = true
+            // otherwise, IF current number is NOT in obj
+                // add current number to obj, set bool = false
+                // IF |i - j| <= k
+                    // change obj[num] = true
+// loop through values in obj
+    // IF any are false
+        // return false
+// return true
+var __containsNearbyDuplicate = function(nums, k) {
+    let obj = {};
+    for (let i = 0; i < nums.length; i++) {
+        let j = i+1;
+        let curNum = nums[i];
+        for (j; j < nums.length; j++) {
+            if (curNum === nums[j]) {
+                if (obj[curNum]) {
+                    if (Math.abs(i - j) <= k) {
+                        obj[curNum] = true;
+                    }
+                } else {
+                    obj[curNum] = false;
+                    if (Math.abs(i - j) <= k) {
+                        obj[curNum] = true;
+                    }
+                }
+            }
+        }
+    }
+    let bools = Object.values(obj);
+    for (let i = 0; i < bools.length; i++) {
+        if (bools[i] === false) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+
+
+// This initial plan does not account for when some duplicates pass all three conditions and not others
+// based on test 2, [1,0,1,1] and k = 1, the first 1 fails 3, but the last 1's, pass. Which I guess counts
 // create a set to hold already checked numbers
 // create two pointer i and j, where i = j+1
 // loop through nums, until i is at the end
@@ -43,7 +160,7 @@ Constraints:
             // save value at i into set
     // i++
 // return true
-var containsNearbyDuplicate = function(nums, k) {
+var _containsNearbyDuplicate = function(nums, k) {
     let numsChecked = new Set();
     let i = 0;
     while (i < nums.length) {
