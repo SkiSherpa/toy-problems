e7120f86538a3c5eb05bb9f99d96f2647aaa5cac
Erick Watanabe <erickjwatanabe@gmail.com>
2024-05-21 15:57:02 -0600

    sum left leaves complete + analysis

---
+++ nishuProbs/easy/sumLeftLeaves.py
@@ -0,0 +1,64 @@
+# Given the root of a binary tree, return the sum of all left leaves.
+
+# A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
+
+
+
+# Example 1:
+
+
+# Input: root = [3,9,20,null,null,15,7]
+# Output: 24
+# Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+# Example 2:
+
+# Input: root = [1]
+# Output: 0
+
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [1, 1000].
+# -1000 <= Node.val <= 1000
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# sum = 0
+# bfs (node, isLeft)
+# declare nonlocal sum
+# if no node return
+# IF isLEft is true and no next node
+    # add val to sum
+    # return
+# IF next left exists
+    # bfs call node.left, true
+# IF next right exists
+    # bfs call node.right, false
+
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        sum = 0
+        def bfs(node, isLeft):
+            nonlocal sum
+            if node == None:
+                return
+            if node.left == None and node.right == None and isLeft == True:
+                sum += node.val
+                return
+            if node.left:
+                bfs(node.left, True)
+            if node.right:
+                bfs(node.right, False)
+
+        bfs(root.left, True)
+        bfs(root.right, False)
+        return sum
+
+# t = O(n), where n is the depth of the tree
+# m = O(n), where n is the depth of the tree
+# t = 28ms 93.50% | m = 16.72 49.10%
