0ef5fea2f1ab7d6159d6bf73fe84b5d3ada9430a
Erick Watanabe <erickjwatanabe@gmail.com>
2024-06-06 10:59:49 -0700

    between6and7 solved

---
+++ nishuProbs/easy/between6and7.js
@@ -0,0 +1,8 @@
+// what should go where ? is to make the result always be between 6 and 7.
+function update(arg) { return Math.random() + y * arg; }
+let y = 2;
+let x = 3;
+const result = update(x);
+console.log(result)
+const res = update(x);
+console.log(res)
